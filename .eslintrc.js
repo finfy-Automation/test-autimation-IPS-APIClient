71157418b5dae8789c3e972c0978df7b44041f43
andreyLobo0 <andrey.lobo@luby.software>
2023-08-24 15:49:41 -0300

    Config: configuração do base do repositorio e lint

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': 'google',
+  'overrides': [
+    {
+      'env': {
+        'node': true,
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+      },
+    },
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2,
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'semi': [
+      'error',
+      'never',
+    ],
+    'no-trailing-spaces': [
+      'error',
+    ],
+    'require-jsdoc': 'off',
+    'max-len': ['error', {'code': 200}],
+  },
+}
